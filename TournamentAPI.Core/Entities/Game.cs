dc8f6353c99b354cdb7a2694d221ab29736132a0
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-05-26 17:29:55 +0200

    Data annotations for titles

--- TournamentAPI.Core/Entities/Game.cs
+++ TournamentAPI.Core/Entities/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace TournamentAPI.Core.Entities
     public class Game
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
         public DateTime time { get; set; }
         public int TournamentId { get; set; }
--- TournamentAPI.Core/Entities/Tournament.cs
+++ TournamentAPI.Core/Entities/Tournament.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace TournamentAPI.Core.Entities
     public class Tournament
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
         public DateTime StartDate { get; set; }
         public ICollection<Game> Games { get; set; }
